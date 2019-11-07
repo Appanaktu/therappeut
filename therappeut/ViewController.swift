e506f786639b8697420dc8bec4013f49733b1ff4
Patrick <patrick.mondani@me.com>
2019-11-07 16:51:20 +0100

    Initial

---
+++ therappeut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  therappeut
+//
+//  Created by Patrick Mondani on 07.11.19.
+//  Copyright © 2019 patzimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
